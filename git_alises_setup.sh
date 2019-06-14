e6449acf29334ae4e1a9da4141c06ea9c48da4f1
Mark Jackson <mark.jackson@starz.com>
2019-06-14 11:31:01 -0700

    adding nuke git alias

--- git_alises_setup.sh
+++ git_alises_setup.sh
@@ -3,3 +3,4 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.nuke '!sh -c "git branch -D $1 && git push origin :$1" -'
