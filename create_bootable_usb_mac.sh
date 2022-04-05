db4b89150fecf70c528a17f2ad2482d92ad590ec
Mark Jackson <markjackson@Marks-MBP-2.tx.rr.com>
2022-04-05 15:42:21 -0500

    making usb boot script runnable
    - also changing the device name to use the full path instead, as it's
      more intuative that way

--- create_bootable_usb_mac.sh
+++ create_bootable_usb_mac.sh
@@ -8,11 +8,11 @@ iso_file_name=$1
 img_file_name=$(echo "${iso_file_name}" | sed s/.iso/.img/g)
 
 # you can find this by running: diskutil list
-# an example of this would be "disk2"
-device_name=$2
+# an example of this would be "/dev/disk2"
+device_path=$2
 
-echo using iso file = "${iso_file_name}" and device = /dev/"${device_name}"
+echo using iso file = "${iso_file_name}" and device = "${device_path}"
 
 hdiutil convert -format UDRW -o "${img_file_name}"  "${iso_file_name}"
 mv "${img_file_name}".dmg "${img_file_name}"
-sudo dd if="${img_file_name}" of=/dev/"${device_name}"
+sudo dd if="${img_file_name}" of="${device_path}"
